f23c0344e1b316a10ba126d481eb0bfb3b01e273
RubyRaven6 <swampertspawner@gmail.com>
2024-12-27 16:00:16 +0800

    Small changes to test out time.

--- include/fake_rtc.h
+++ include/fake_rtc.h
@@ -6,7 +6,8 @@
 struct Time* FakeRtc_GetCurrentTime(void);
 void FakeRtc_GetRawInfo(struct SiiRtcInfo *rtc);
 void FakeRtc_AdvanceTimeBy(u32 hours, u32 minutes, u32 seconds);
-void FakeRtc_ManuallySetTime(u32 dayOfWeek, u32 hour, u32 minute, u32 second);
+void FakeRtc_ManuallySetTime(u32 hour, u32 minute, u32 second);
+void FakeRtc_ManuallySetDayOfWeek(u32 dayOfWeek);
 void FakeRtc_TickTimeForward(void);
 u32 FakeRtc_GetSecondsRatio(void);
 void FakeRtc_CalcTimeDifference(struct Time *result, struct Time *t1, struct Time *t2);
