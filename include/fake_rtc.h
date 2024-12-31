1b22074ab1606bd8e201da6829828d30ada14629
reynaldovalvarez <r.alvarez@RAlvarezs-iMac.local>
2024-12-31 19:26:00 +0800

    Finally able to say that the time can now be modified easily; good jumping off point for making macros.

--- include/fake_rtc.h
+++ include/fake_rtc.h
@@ -10,7 +10,5 @@ void FakeRtc_AdvanceTimeBy(u32 days, u32 hours, u32 minutes, u32 seconds);
 void FakeRtc_ForceSetTime(u32 day, u32 hour, u32 minute, u32 second);
 void FakeRtc_TickTimeForward(void);
 u32 FakeRtc_GetSecondsRatio(void);
-void FakeRtc_CalcTimeDifference(struct Time *result, struct Time *t1, struct Time *t2);
-void Script_ToggleFakeRtc(void);
 
 #endif // GUARD_FAKE_RTC_UTIL_H
