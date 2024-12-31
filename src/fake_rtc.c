cfec64cf08e2e6a2fd54f11a558bee535b8727e1
RubyRaven6 <swampertspawner@gmail.com>
2024-12-31 19:26:00 +0800

    Finally able to say that the time can now be modified easily; good jumping off point for making macros.

--- src/fake_rtc.c
+++ src/fake_rtc.c
@@ -7,8 +7,9 @@
 #include "event_data.h"
 #include "script.h"
 
-static void Script_ToggleFakeRtc(void);
-static void FakeRtc_CalcTimeDifference(struct SiiRtcInfo *result, struct Time *t1, struct SiiRtcInfo *t2);
+static void Script_PauseFakeRtc(void);
+static void Script_ResumeFakeRtc(void);
+static void FakeRtc_CalcTimeDifference(struct Time *result, struct SiiRtcInfo *t1, struct Time *t2);
 
 void FakeRtc_Reset(void)
 {
@@ -22,7 +23,7 @@ void FakeRtc_Reset(void)
 
 struct SiiRtcInfo *FakeRtc_GetCurrentTime(void)
 {
-#if OW_USE_FAKE_RTC == TRUE
+#if OW_USE_FAKE_RTC
     return &gSaveBlock3Ptr->fakeRTC;
 #else
     return NULL;
@@ -47,7 +48,7 @@ void FakeRtc_TickTimeForward(void)
     FakeRtc_AdvanceTimeBy(0, 0, 0, FakeRtc_GetSecondsRatio());
 }
 
-static void FakeRtc_AdvanceSeconds(struct SiiRtcInfo *rtc, u32 *days, u32*hours, u32 *minutes, u32 *seconds)
+static void FakeRtc_AdvanceSeconds(struct SiiRtcInfo *rtc, u32 *days, u32 *hours, u32 *minutes, u32 *seconds)
 {
     *seconds += rtc->second;
     *minutes += rtc->minute;
@@ -81,7 +82,7 @@ static void FakeRtc_SetDayOfWeek(struct SiiRtcInfo *rtc, u32 daysToAdd)
 
 static void FakeRtc_AdvanceDays(struct SiiRtcInfo *rtc, u32 *days)
 {
-    Script_ToggleFakeRtc();
+    Script_PauseFakeRtc();
     u32 remainingDaysInMonth = (sNumDaysInMonths[rtc->month - 1] + (rtc->month == MONTH_FEB && IsLeapYear(rtc->year)) - rtc->day);
     if (*days > remainingDaysInMonth)
     {
@@ -101,59 +102,61 @@ static void FakeRtc_AdvanceDays(struct SiiRtcInfo *rtc, u32 *days)
         FakeRtc_SetDayOfWeek(rtc, *days);
         *days = 0;
     }
-    Script_ToggleFakeRtc();
+    Script_ResumeFakeRtc();
 }
 
 void FakeRtc_AdvanceTimeBy(u32 days, u32 hours, u32 minutes, u32 seconds)
 {
-    Script_ToggleFakeRtc();
+    Script_PauseFakeRtc();
     struct SiiRtcInfo *rtc = FakeRtc_GetCurrentTime();
     FakeRtc_AdvanceSeconds(rtc, &days, &hours, &minutes, &seconds);
 
     while (days > 0)
         FakeRtc_AdvanceDays(rtc, &days);
-    Script_ToggleFakeRtc();
+    Script_ResumeFakeRtc();
 }
 
-void FakeRtc_ForwardTimeTo(u32 day, u32 hour, u32 minute, u32 second)
+void FakeRtc_ForwardTimeTo(u32 hour, u32 minute, u32 second)
 {
-    Script_ToggleFakeRtc();
-    struct SiiRtcInfo diff, target;
-    RtcCalcLocalTime();
+    Script_PauseFakeRtc();
+    struct Time diff, target;
+    struct SiiRtcInfo *fakeRtc = FakeRtc_GetCurrentTime();
 
-    target.hour = hour;
-    target.minute = minute;
-    target.second = second;
-    target.day = day;
+    target.hours = hour;
+    target.minutes = minute;
+    target.seconds = second;
 
-    FakeRtc_CalcTimeDifference(&diff, &gLocalTime, &target);
-    FakeRtc_AdvanceTimeBy(diff.day, diff.hour, diff.minute, diff.second);
-    Script_ToggleFakeRtc();
+    FakeRtc_CalcTimeDifference(&diff, fakeRtc, &target);
+
+    FakeRtc_AdvanceTimeBy(0, diff.hours, diff.minutes, diff.seconds);
+
+    Script_ResumeFakeRtc();
 }
 
-void FakeRtc_CalcTimeDifference(struct SiiRtcInfo *result, struct Time *t1, struct SiiRtcInfo *t2)
+
+static void FakeRtc_CalcTimeDifference(struct Time *result, struct SiiRtcInfo *t1, struct Time *t2)
 {
-    result->second = t2->second - t1->seconds;
-    result->minute = t2->minute - t1->minutes;
-    result->hour = t2->hour - t1->hours;
-    result->day = t2->day - t1->days;
+    result->seconds = t2->seconds - t1->second;
+    result->minutes = t2->minutes - t1->minute;
+    result->hours = t2->hours - t1->hour;
+    result->days = t2->days - t1->day;
 
-    if (result->second < 0)
+    if (result->seconds < 0)
     {
-        result->second += SECONDS_PER_MINUTE;
-        --result->minute;
+        result->seconds += SECONDS_PER_MINUTE;
+        --result->minutes;
     }
 
-    if (result->minute < 0)
+    if (result->minutes < 0)
     {
-        result->minute += MINUTES_PER_HOUR;
-        --result->hour;
+        result->minutes += MINUTES_PER_HOUR;
+        --result->hours;
     }
 
-    if (result->hour < 0)
+    if (result->hours < 0)
     {
-        result->hour += HOURS_PER_DAY;
-        --result->day;
+        result->hours += HOURS_PER_DAY;
+        --result->days;
     }
 }
 
@@ -162,11 +165,6 @@ void FakeRtc_ForceSetTime(u32 day, u32 hour, u32 minute, u32 second)
     FakeRtc_Reset();
     FakeRtc_AdvanceTimeBy(day, hour, minute, second);
 }
-
-// void AdvanceScript(void)
-// {
-//     FakeRtc_AdvanceTimeBy(300, 0, 0, 0);
-// }
 
 u32 FakeRtc_GetSecondsRatio(void)
 {
@@ -188,31 +186,70 @@ void Script_ResumeFakeRtc(void)
     FlagClear(OW_FLAG_PAUSE_TIME);
 }
 
-static void Script_ToggleFakeRtc(void)
+void Script_ToggleFakeRtc(void)
 {
     FlagToggle(OW_FLAG_PAUSE_TIME);
 }
 
 bool8 ScrCmd_addtime(struct ScriptContext *ctx)
 {
-    u32 addedhours = ScriptReadWord(ctx);
-    u32 addedminutes = ScriptReadWord(ctx);
-    FakeRtc_AdvanceTimeBy(addedhours, addedminutes, 0);
+    u32 addedDays = ScriptReadWord(ctx);
+    u32 addedHours = ScriptReadWord(ctx);
+    u32 addedMinutes = ScriptReadWord(ctx);
+    FakeRtc_AdvanceTimeBy(addedDays, addedHours, addedMinutes, 0);
     return FALSE;
 }
 
-bool8 ScrCmd_settime(struct ScriptContext *ctx)
+bool8 ScrCmd_forwardtime(struct ScriptContext *ctx)
 {
-    //u32 setDayOfWeek = ScriptReadWord(ctx);
-    u32 setHour = ScriptReadWord(ctx);
-    u32 setMinute = ScriptReadWord(ctx);
-    FakeRtc_ManuallySetTime(setHour, setMinute, 0);
+    // u32 setDayOfWeek = ScriptReadWord(ctx);
+    // u32 setHour = ScriptReadWord(ctx);
+    // u32 setMinute = ScriptReadWord(ctx);
     return FALSE;
 }
 
 bool8 ScrCmd_setdayofweek(struct ScriptContext *ctx)
 {
-    u32 setDayOfWeek = ScriptReadWord(ctx);
-    FakeRtc_ManuallySetDayOfWeek(setDayOfWeek);
     return FALSE;
+}
+
+///////////////////////////////////
+//////// TESTING FUNCTIONS ////////
+///////////////////////////////////
+
+void Script_Testing1(void)
+{
+    struct SiiRtcInfo *fakeRtc = FakeRtc_GetCurrentTime();
+    MgbaPrintf(MGBA_LOG_WARN, "Days Before AdvanceTime runs: %u", fakeRtc->day);
+    MgbaPrintf(MGBA_LOG_WARN, "Day of the week Before AdvanceTime runs: %u", fakeRtc->dayOfWeek);
+    MgbaPrintf(MGBA_LOG_WARN, "Hours Before AdvanceTime runs: %u", fakeRtc->hour);
+    MgbaPrintf(MGBA_LOG_WARN, "Minutes Before AdvanceTime runs: %u", fakeRtc->minute);
+    FakeRtc_AdvanceTimeBy(1, 6, 6, 0);
+    MgbaPrintf(MGBA_LOG_WARN, "Days After AdvanceTime runs: %u", fakeRtc->day);
+    MgbaPrintf(MGBA_LOG_WARN, "Day of the week After AdvanceTime runs: %u", fakeRtc->dayOfWeek);
+    MgbaPrintf(MGBA_LOG_WARN, "Hours After AdvanceTime runs: %u", fakeRtc->hour);
+    MgbaPrintf(MGBA_LOG_WARN, "Minutes After AdvanceTime runs: %u", fakeRtc->minute);
+}
+
+void Script_Testing2(void)
+{
+    //struct SiiRtcInfo *rtc = FakeRtc_GetCurrentTime();
+    // MgbaPrintf(MGBA_LOG_WARN, "Hour: %u", rtc->hour);
+    // MgbaPrintf(MGBA_LOG_WARN, "Minute: %u", rtc->minute);
+    FakeRtc_ForwardTimeTo(20, 12, 0);
+    // MgbaPrintf(MGBA_LOG_WARN, "Hour: %u", rtc->hour);
+    // MgbaPrintf(MGBA_LOG_WARN, "Minute: %u", rtc->minute);
+}
+
+void Script_Testing3(void)
+{
+    struct SiiRtcInfo *rtc = FakeRtc_GetCurrentTime();
+    
+    u32 weekdayTarget = 5;
+    u32 weekdayCurrent = rtc->dayOfWeek;
+    u32 daysToAdd;
+
+    daysToAdd = ((weekdayTarget - weekdayCurrent) + 7) % 7;
+
+    FakeRtc_AdvanceTimeBy(daysToAdd, 0, 0, 0);
 }
