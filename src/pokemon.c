11ebaeea3a249ab31834bbbfa70bf0b9230b2289
RubyRaven6 <swampertspawner@gmail.com>
2025-05-10 17:00:29 +0800

    Cooking with diesel

--- src/pokemon.c
+++ src/pokemon.c
@@ -1756,7 +1756,7 @@ static u16 CalculateBoxMonChecksum(struct BoxPokemon *boxMon)
 #define CALC_STAT(base, iv, ev, statIndex, field)               \
 {                                                               \
     u8 baseStat = gSpeciesInfo[species].base;                   \
-    s32 n = (((2 * baseStat + iv + ev) * level) / 100) + 5; \
+    s32 n = (((2 * baseStat + iv) * level) / 100) + (5 + ev); \
     n = ModifyStatByNature(nature, n, statIndex);               \
     if (B_FRIENDSHIP_BOOST == TRUE)                             \
         n = n + ((n * 10 * friendship) / (MAX_FRIENDSHIP * 100));\
