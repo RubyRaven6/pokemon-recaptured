af0bb8039cbe095c7df0c69437985533f447d185
Ruby <swampertspawner@gmail.com>
2026-08-26 16:22:36 +0800

    Miscellaneous changes (#2)
    * dewford town change
    
    * imported general tileset to porytiles
    
    * commit tileset manifest
    
    * updated .github
    
    * save test updated

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1738,3 +1738,9 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
 	.include "data/scripts/wild_encounter.inc"
+
+	.include "data/maps/Dewford_FillerMap/scripts.inc"
+
+	.include "data/maps/DewfordTown_Harbor/scripts.inc"
+
+	.include "data/maps/DewfordTown_Mart/scripts.inc"
